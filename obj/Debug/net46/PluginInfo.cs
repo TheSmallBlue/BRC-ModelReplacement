4f556cbdac54237b949e2a6fab6a7633c2c54df1
TheSmallBlue <ilanmori@hotmail.com>
2023-08-23 01:52:24 -0300

    .gitignore is now working

--- obj/Debug/net46/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ModelReplacement
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "ModelReplacement";
-        public const string PLUGIN_NAME = "ModelReplacement";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
