d2da4d511292dcf5436c326ca4381c6d1c44d3ca
TheSmallBlue <ilanmori@hotmail.com>
2023-08-31 02:17:43 -0300

    Allow for skate localposition and localscale changes

--- Plugin.cs
+++ Plugin.cs
@@ -14,15 +14,22 @@ namespace ModelReplacement
     public class Plugin : BaseUnityPlugin
     {
         public static ConfigEntry<int> configCharacterToReplace;
-        public static ConfigEntry<Vector3> inlineSkatesRotL;
-        public static ConfigEntry<Vector3> inlineSkatesRotR;
+        public static ConfigEntry<Vector3> inlineSkatesRotL, inlineSkatesPosL, inlineSkatesScaleL;
+        public static ConfigEntry<Vector3> inlineSkatesRotR, inlineSkatesPosR, inlineSkatesScaleR;
         public static ConfigEntry<bool> configOverwriteShader;
 
         private void Awake()
         {
             configCharacterToReplace = Config.Bind("General", "charaToReplace", -1, "Which character to replace, taken from the 'Characters' enum. See this image (The numbers are one digit more than they should be, so what would be 2 in this image is actually 1, 3 is 2, 4 is 3, and so on): https://files.catbox.moe/vhda8a.png");
+
             inlineSkatesRotL = Config.Bind("General", "inlineSkatesDirL", Vector3.zero, "The rotation of the left inline skate in angles. Modify this to make your left skate look correct");
+            inlineSkatesPosL = Config.Bind("General", "inlineSkatesPosL", Vector3.zero, "The Position of the left inline skate relative to the leg bone. Modify this to make your left skate look correct");
+            inlineSkatesScaleL = Config.Bind("General", "inlineSkatesScaleL", Vector3.zero, "The scale of the left inline skate relative to the leg bone. Modify this to make your left skate look correct");
+
             inlineSkatesRotR = Config.Bind("General", "inlineSkatesDirR", Vector3.zero, "The rotation of the right inline skate in angles. Modify this to make your right skate look correct");
+            inlineSkatesPosR = Config.Bind("General", "inlineSkatesPosR", Vector3.zero, "The Position of the right inline skate relative to the leg bone. Modify this to make your left skate look correct");
+            inlineSkatesScaleR = Config.Bind("General", "inlineSkatesScaleR", Vector3.zero, "The scale of the right inline skate relative to the leg bone.  Modify this to make your left skate look correct");
+
             configOverwriteShader = Config.Bind("General", "shaderOverwritten", false, "Whether or not we prioritize the shader you set to your material in the Unity editor or the base shader the game uses for outlines and cel-shading");
 
             SavedVariables.charaPrefab = SavedVariables.GetBundle().LoadAsset<GameObject>("Chara");
@@ -122,8 +129,13 @@ namespace ModelReplacement
         static void Postfix(CharacterVisual.MoveStylePropMode mode, PlayerMoveStyleProps ___moveStyleProps, CharacterVisual __instance){
             
             if(mode == CharacterVisual.MoveStylePropMode.ACTIVE && __instance.transform.GetChild(0).name.Contains("Chara")){
+                ___moveStyleProps.skateL.transform.localPosition = Plugin.inlineSkatesPosL.Value;
                 ___moveStyleProps.skateL.transform.localRotation = Quaternion.Euler(Plugin.inlineSkatesRotL.Value);
+                ___moveStyleProps.skateL.transform.localScale = Plugin.inlineSkatesScaleL.Value;
+
+                ___moveStyleProps.skateR.transform.localPosition = Plugin.inlineSkatesPosR.Value;
                 ___moveStyleProps.skateR.transform.localRotation = Quaternion.Euler(Plugin.inlineSkatesRotR.Value);
+                ___moveStyleProps.skateR.transform.localScale = Plugin.inlineSkatesScaleR.Value;
             }
         }
 
